9236c0b115e6af4da6b7e2c3281adf81bffcf608
CrinitusFeles <crinitusfeles@gmail.com>
2025-12-07 15:48:48 +0700

    fixed spinner

--- qcustomwidgets/widgets/spinner.py
+++ qcustomwidgets/widgets/spinner.py
@@ -7,7 +7,7 @@ from PyQt6.QtGui import QPainter, QPen, QColor
 
 
 class Spinner(QWidget):
-    def __init__(self, line_width: int, color: str) -> None:
+    def __init__(self, line_width: int, color: str, size: int) -> None:
         super().__init__()
         self.w: int = line_width
         self.color = QColor(color)
@@ -15,6 +15,10 @@ class Spinner(QWidget):
         self.speed = 6
         self.play = True
         self.last_call: float = time.time()
+        self.setMinimumWidth(size)
+        self.setMinimumHeight(size)
+        self.setMaximumHeight(size)
+        self.setMaximumWidth(size)
 
     @override
     def paintEvent(self, a0) -> None:
@@ -24,9 +28,10 @@ class Spinner(QWidget):
         w: int = self.w
         pen = QPen(self.color, w)
         pt.setPen(pen)
+        s = min(self.width(), self.height())
         alen: float = ((sin(radians(self.angle / 16)) + 1) / 2) * (180 * 16)
         alen += ((sin(radians((self.angle / 16) + 130)) + 1) / 2) * (180 * 16)
-        pt.drawArc(w, w, self.width() - w * 2, self.height() - w * 2,
+        pt.drawArc(w, w, s - w * 2, s - w * 2,
                    self.angle, int(alen))
         pt.end()
 
@@ -45,6 +50,6 @@ class Spinner(QWidget):
 
 if __name__ == '__main__':
     app = QApplication([])
-    w = Spinner(16, 'green')
+    w = Spinner(4, 'green', 25)
     w.show()
     app.exec()
