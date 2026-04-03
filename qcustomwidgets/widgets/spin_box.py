316eb19ea257d1d5d0365dbadef9269a1c2e0246
CrinitusFeles <crinitusfeles@gmail.com>
2026-04-03 11:29:50 +0700

    added hex mode for SpinBox

--- qcustomwidgets/widgets/spin_box.py
+++ qcustomwidgets/widgets/spin_box.py
@@ -1,5 +1,7 @@
 from PyQt6 import QtWidgets, QtGui, QtCore
-class MySpinBox(QtWidgets.QAbstractSpinBox):
+
+
+class SpinBox(QtWidgets.QAbstractSpinBox):
     def __init__(self, parent: QtWidgets.QWidget | None = None) -> None:
         super().__init__(parent)
         self.editingFinished.connect(self.on_editing_finished)
@@ -7,81 +9,72 @@ class MySpinBox(QtWidgets.QAbstractSpinBox):
         self._line_edit: QtWidgets.QLineEdit = self.lineEdit()  # type: ignore
         self.step_size: int = 1
         self.min_val: int = 0
-        self.max_val: int = 999999
+        self.max_val: int = 0xFFFFFFFF
+        self.is_hex_mode: bool = False
+        QtGui.QShortcut(QtGui.QKeySequence('Ctrl+Q'), self,
+                                           self.toggle_hex_mode,
+                                           context=QtCore.Qt.ShortcutContext.WidgetShortcut)
 
-    # def mousePressEvent(self,  e):
-    #     super().mousePressEvent(e)
-    #     if e:
-    #         opt = QtWidgets.QStyleOptionSpinBox()
-    #         self.initStyleOption(opt)
-    #         control = self.style().hitTestComplexControl(  # type: ignore
-    #             QtWidgets.QStyle.ComplexControl.CC_SpinBox, opt, e.pos(), self
-    #         )
-    #         if control == QtWidgets.QStyle.SubControl.SC_SpinBoxUp:
-    #             # self.upClicked.emit()
-    #             self.stepBy(self.step_size)
-    #         elif control == QtWidgets.QStyle.SubControl.SC_SpinBoxDown:
-    #             # self.downClicked.emit()
-    #             self.stepBy(-self.step_size)
+    def toggle_hex_mode(self) -> None:
+        self.is_hex_mode = not self.is_hex_mode
+        self._set_val(self.last_valid_val)
 
     def stepEnabled(self) -> QtWidgets.QAbstractSpinBox.StepEnabledFlag:
-        super().stepEnabled()
         if self.last_valid_val != self.max_val and self.last_valid_val != self.min_val:
             return super().StepEnabledFlag.StepDownEnabled | super().StepEnabledFlag.StepUpEnabled
         elif self.last_valid_val == self.max_val:
             return super().StepEnabledFlag.StepDownEnabled
-        elif self.last_valid_val == self.max_val:
+        elif self.last_valid_val == self.min_val:
             return super().StepEnabledFlag.StepUpEnabled
         else:
             return super().StepEnabledFlag.StepNone
 
-    def on_editing_finished(self):
-        text = self.text()
+    def on_editing_finished(self) -> None:
+        text: str = self.text()
         try:
             val = eval(text)
-            if isinstance(val, (int, float)) and self.min_val <= val <= self.max_val:
-                self._line_edit.setText(f'{int(val)}')
-                self.last_valid_val = int(val)
+            if isinstance(val, (int, float)):
+                self.setValue(int(val))
             else:
                 raise ValueError
         except Exception:
-            self._line_edit.setText(f'{self.last_valid_val}')
+            self._set_val(self.last_valid_val)
+
+    def _set_val(self, val: int) -> None:
+        if self.is_hex_mode:
+            if val > 0:
+                self._line_edit.setText(f'0x{val:02X}')
+            else:
+                self._line_edit.setText(f'- 0x{-val:02X}')
+        else:
+            self._line_edit.setText(f'{val}')
 
     def focusOutEvent(self, e: QtGui.QFocusEvent | None) -> None:
         self.on_editing_finished()
         return super().focusOutEvent(e)
 
-    def wheelEvent(self, e: QtGui.QWheelEvent | None) -> None:
-        if e:
-            steps = 1 if e.angleDelta().y() > 0 else -1
-            if self.min_val <= self.last_valid_val + steps <= self.max_val:
-                self.last_valid_val += steps
-                self._line_edit.setText(f'{self.last_valid_val}')
-        return super().wheelEvent(e)
-
     def stepBy(self, steps: int) -> None:
         if self.min_val <= self.last_valid_val + steps <= self.max_val:
             self.last_valid_val += steps
-            self._line_edit.setText(f'{self.last_valid_val}')
-        super().stepBy(steps)
+            self._set_val(self.last_valid_val)
 
-    def keyPressEvent(self, e: QtGui.QKeyEvent | None):
-        if e:
-            if e.key() == QtCore.Qt.Key.Key_Up:
-                self.stepUp()
-            elif e.key() == QtCore.Qt.Key.Key_Down:
-                self.stepDown()
-        super().keyPressEvent(e)
-
-    def setRange(self, min_val: int, max_val: int):
+    def setRange(self, min_val: int, max_val: int) -> None:
         self.setMinimum(min_val)
         self.setMaximum(max_val)
 
-    def setMinimum(self, min_val: int):
+    def setMinimum(self, min_val: int) -> None:
         self.min_val = min_val
 
-    def setMaximum(self, max_val: int):
+    def setMaximum(self, max_val: int) -> None:
         self.max_val = max_val
+
+    def setValue(self, val: int) -> None:
+        if self.min_val <= val <= self.max_val:
+            self.last_valid_val = val
+            self._set_val(val)
+
+    def value(self) -> int:
+        return self.last_valid_val
 
 
 if __name__ == '__main__':
@@ -89,8 +82,8 @@ if __name__ == '__main__':
     w = QtWidgets.QWidget()
     _l = QtWidgets.QVBoxLayout(w)
     w.setLayout(_l)
-    _l.addWidget(MySpinBox())
-    _l.addWidget(MySpinBox())
+    _l.addWidget(SpinBox())
+    _l.addWidget(SpinBox())
     _l.addWidget(QtWidgets.QSpinBox())
     w.show()
     app.exec()
